79affae217f265a93aa9a973827efca769b11ad4
Michał Krasoń <kramichal96@gmail.com>
2017-05-07 10:33:30 +0200

    added timer
    added timer to end if given time is exceeded

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include "tsp.hpp"
 
+#include <iostream>
+#include <ctime>
+#include <csignal>
+#include <wait.h>
+#include <unistd.h>
+
 using namespace std;
 
 int main(int argc, char const *argv[]) {
+  time_t start = time(NULL);
   initializeSearch();
-  search();
+  int limit;
+  cin >> limit;
+  pid_t pid = fork();
+  if (pid == 0) {
+    search();
+  } else {
+    time_t currentTime;
+    while (1) {
+      sleep(1);
+      currentTime = time(NULL);
+      if (currentTime - start >= limit) {
+        kill(pid, SIGINT);
+        break;
+      }
+    }
+    wait(NULL);
+  }
   return 0;
 }
